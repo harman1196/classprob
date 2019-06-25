be1f5949c3e1ed3f6014e22c9dedc44054e73220
747624 <747624@123-PC20.student.Tor.Loc>
2019-06-25 15:18:20 -0400

    assert equql test

--- src/week08/CircleTest.java
+++ src/week08/CircleTest.java
@@ -7,8 +7,57 @@ import org.junit.Test;
 public class CircleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructer() {
+		Circle c=new Circle(5);
+		
+		double expctradius=5;
+		double actradius=c.getRadius();
+		assertEquals(expctradius, actradius,0.01);	
+		
 	}
 
+	@Test
+	public void testAreaFunction() {
+		Circle c=new Circle(5);
+		double expctarea=Math.PI*5*5;
+		double actarea=c.getArea();
+		assertEquals(expctarea, actarea,0.01);	
+	}
+
+	@Test
+	public void testCircumfrenceFunction() {
+		Circle c=new Circle(5);
+		double expctcircumfrence=Math.PI*2*5;
+		double actcircumfrence=c.getCircumfrence();
+		assertEquals(expctcircumfrence, actcircumfrence,0.01);		
+	}
+
+	@Test
+	public void testDiameterFunction() {
+		Circle c=new Circle(5);
+		double expctdiameter=10;
+		double actdiameter=c.getDiameter();
+		assertEquals( expctdiameter, actdiameter,0.01);
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
